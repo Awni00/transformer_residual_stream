96ac9461d9e002cd34a08b9ca8c2a4a3f1aaa900
Awni <awnyaltabaa@gmail.com>
2024-07-06 17:37:57 -0400

    added attn/ffn/etc to make module completely self-contained

--- dual_attention_transformer.py
+++ dual_attention_transformer.py
@@ -5,11 +5,6 @@ import model_utils
 import math
 from einops import rearrange
 
-from attention import Attention
-from attention_utils import repeat_kv, apply_rotary_emb, compute_causal_mask, precompute_freqs_cis
-from positional_encoding import RelativePositionalEncoding
-from transformer_blocks import FeedForwardBlock, create_norm
-
 # An implementation of Dual Attention as proposed in the paper
 # "Disentangling and Integrating Relational and Sensory Information in Transformer Architectures"
 # Awni Altabaa, John Lafferty (2024). https://arxiv.org/abs/2405.16727
@@ -19,7 +14,6 @@ from transformer_blocks import FeedForwardBlock, create_norm
 # the second type is relational attention, which captures relational features.
 
 # DualAttention is a concatenation of self-attention and relational attention heads.
-
 class DualAttention(nn.Module):
     def __init__(self,
         d_model: int,
@@ -141,6 +135,7 @@ class DualAttention(nn.Module):
             rel_attn_scores = None
 
         return out, self_attn_scores, rel_attn_scores
+
 
 
 # Implementation of RelationalAttention as proposed in
@@ -558,6 +553,59 @@ class PositionRelativeSymbolRetriever(nn.Module):
     def forward(self, x):
         length = x.shape[1]
         return self.rel_pos_enc(length)
+
+class RelativePositionalEncoding(nn.Module):
+
+    def __init__(self, dim: int, max_rel_pos: int):
+        """
+        module which returns relative positional embeddings for a given pair of sequences.
+
+        I.e., returns tensor whose [i,j]-th entry is the embedding of the relative position "j-i"
+
+        Parameters
+        ----------
+        dim : int
+            dimension of embeddings
+        max_rel_pos : int
+            maximum relative position in either direction (used for clipping)
+        """
+
+        super().__init__()
+        self.num_units = dim
+        self.max_relative_position = max_rel_pos
+        self.rel_pos_embeddings = nn.Parameter(torch.Tensor(max_rel_pos * 2 + 1, dim))
+        nn.init.xavier_uniform_(self.rel_pos_embeddings)
+
+    def forward(self, length_q, length_k=None):
+        """
+        Parameters
+        ----------
+        length_q : int
+            length of query sequence
+        length_k : _type_, optional
+            length of key sequence, by default None
+
+        Returns
+        -------
+        Tensor
+            tensor of shape [length_q, length_k, dim] where [i,j] is the embedding of the relative position "j-i"
+        """
+
+        if length_k is None:
+            length_k = length_q
+
+        range_q = torch.arange(length_q) # TODO: need to set dtype or device?
+        range_k = torch.arange(length_k)
+
+        distance_mat = range_k[None, :] - range_q[:, None]
+        distance_mat_clipped = torch.clamp(distance_mat, -self.max_relative_position, self.max_relative_position)
+
+        final_mat = distance_mat_clipped + self.max_relative_position
+
+        # final_mat = torch.LongTensor(final_mat).cuda()
+        embeddings = self.rel_pos_embeddings[final_mat] #.cuda()
+
+        return embeddings
 
 # endregion
 
@@ -1048,5 +1096,330 @@ def configure_optimizers(model, weight_decay, learning_rate, betas, device_type)
     print(f"using fused AdamW: {use_fused}")
 
     return optimizer
+
+# endregion
+
+# region standard Transformer modules & utility functions
+class Attention(nn.Module):
+    def __init__(self,
+            d_model: int,
+            n_heads: int,
+            dropout: float,
+            key_dim: int = None,
+            n_kv_heads: int = None,
+            add_bias_kv: bool = False,
+            add_bias_out: bool = False,
+            total_n_heads: int = None):
+        """
+        An implementation of Attention with some added customization.
+
+        Allows multi-query attention/grouped query attention, rotary positional embeddings,
+        and custom relation activation functions.
+
+        Parameters
+        ----------
+        d_model : int
+            model dimension
+        n_heads : int
+            number of heads (query heads if n_kv_heads is set)
+        dropout : float
+            dropout rate
+        n_kv_heads : int, optional
+            number of key/value heads. used to implement multi-query attention or grouped query attention.
+            n_kv_heads=1 corresponds to MQA, n_kv_heads > 1 corresponsd to grouped query attention.
+            n_kv_heads=n_heads is standard MHA. uses MHA when None. By default None
+        add_bias_kv : bool, optional
+            whether to use bias in key/value projections, by default False
+        add_bias_out : bool, optional
+            whether to use bias in out projection, by default False
+        total_n_heads : int, optional
+            total number of heads in dual attention (if using dual attention).
+            used to ensure that concat(A, E) is of dimension d_model after concatentation.
+            hence, output dimension is (d_model // total_heads) * n_heads.
+            if None, total_heads = n_heads and output dimension is d_model
+        """
+
+        super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads # number of heads (for query)
+        self.n_kv_heads = n_heads if n_kv_heads is None else n_kv_heads # n_kv_heads = 1 corresponds to multi-query attn
+        self.dropout = dropout
+        self.add_bias_kv = add_bias_kv
+        self.add_bias_out = add_bias_out
+        self.total_n_heads = n_heads if total_n_heads is None else total_n_heads # compatibility for dual attention
+
+        self.key_dim = key_dim if key_dim is not None else self.d_model // self.total_n_heads # key dimension
+        self.n_rep_kv = self.n_heads // self.n_kv_heads # use same kv heads for several query heads
+        self.head_dim = self.d_model // self.total_n_heads # dim of projections
+        assert self.n_heads % self.n_kv_heads == 0 # make sure n_kv_heads fits into n_heads (i.e., can be grouped)
+        assert self.n_rep_kv * self.n_kv_heads == self.n_heads
+        assert self.total_n_heads * self.head_dim == self.d_model
+
+        self.attn_scale = 1 / math.sqrt(self.head_dim) # for scaled dot product attention
+
+        self.wq = nn.Linear(self.d_model, self.n_heads * self.key_dim, bias=False)
+        self.wk = nn.Linear(self.d_model, self.n_kv_heads * self.key_dim, bias=self.add_bias_kv)
+        self.wv = nn.Linear(self.d_model, self.n_kv_heads * self.head_dim, bias=self.add_bias_kv)
+        self.wo = nn.Linear(self.n_heads * self.head_dim, self.n_heads * self.head_dim, bias=self.add_bias_out)
+        self.attn_dropout = nn.Dropout(self.dropout)
+        self.resid_dropout = nn.Dropout(self.dropout)
+
+    def forward(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        freqs_cos: torch.Tensor = None,
+        freqs_sin: torch.Tensor = None,
+        attn_mask: torch.Tensor = None, # boolean attention mask: True indicates corresponding position *should* be attended to
+        is_causal: bool = False, # indicates causal mask; should only set one of is_causal and attn_mask
+        need_weights: bool = False
+    ):
+        """
+        compute attention with given query, key, value.
+
+        if freqs_cos and freqs_sin are given, apply rotary positional embeddings.
+        if attn_mask is given, apply attention mask.
+        if is_causal is True, apply causal mask (attn_mask must be None).
+
+        Parameters
+        ----------
+        query : torch.Tensor
+            query sequence of shape [bsz, len_in, d_model]
+        key : torch.Tensor
+            key sequence of shape [bsz, len_ctx, d_model]
+        value : torch.Tensor
+            value sequence of shape [bsz, len_ctx, d_model]
+        freqs_cos : torch.Tensor, optional
+            cosine of frequencies for RoPE. RoPE is applied if given.
+            Note: RoPE does not work for cross-attention. By default None
+        freqs_sin : torch.Tensor, optional
+            cosine of frequencies for RoPE. RoPE is applied if given.
+            Note: RoPE does not work for cross-attention. By default None
+        attn_mask : torch.Tensor, optional
+            boolean attention mask of shape [len_in, len_ctx]. True at [i,j] indicates i is allowed to attend to j.
+            By default None
+        is_causal : bool, optional
+            whether to apply a causal mask. If True, attn_mask must be None. Only applies for self-attention.
+            By default False
+        need_weights : bool, optional
+            whether to return the attention scores. If True, return value will be tuple (output, attn_scores).
+            If True, will compute attention manually rather than using flash attention. By default False
+
+        Returns
+        -------
+        torch.Tensor
+            result of attention
+        """
+
+        bsz, qseqlen, _ = query.shape
+        bsz, kseqlen, _ = key.shape
+        bsz, vseqlen, _ = value.shape
+        assert kseqlen == vseqlen
+
+        # apply query/key/value projections and reshape to split into different heads
+        xq, xk, xv = self.wq(query), self.wk(key), self.wv(value)
+        xq = xq.view(bsz, qseqlen, self.n_heads, self.key_dim)
+        xk = xk.view(bsz, kseqlen, self.n_kv_heads, self.key_dim)
+        xv = xv.view(bsz, vseqlen, self.n_kv_heads, self.head_dim)
+
+        # apply RoPE relative positional embeddings (if given)
+        if freqs_cos is not None and freqs_sin is not None:
+            xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
+
+        # grouped multiquery attention: expand out keys and values
+        if self.n_rep_kv != 1:
+            xk = repeat_kv(xk, self.n_rep_kv)  # (bs, seqlen, n_heads, key_dim)
+            xv = repeat_kv(xv, self.n_rep_kv)  # (bs, seqlen, n_heads, head_dim)
+
+        # make heads into a batch dimension
+        xq = xq.transpose(1, 2)  # (bs, n_heads, seqlen, key_dim)
+        xk = xk.transpose(1, 2)
+        xv = xv.transpose(1, 2)
+
+        # use flash implementation for softmax activation if weights not needed
+        if not need_weights:
+            output = torch.nn.functional.scaled_dot_product_attention(
+                xq, xk, xv, attn_mask=attn_mask, dropout_p=self.dropout if self.training else 0.0, is_causal=is_causal, scale=self.attn_scale)
+            scores = None
+
+        # manual implementation for other kinds of activation
+        else:
+            # generate causal attn_mask
+            assert not (attn_mask is not None and is_causal)
+            if is_causal and attn_mask is None:
+                attn_mask = compute_causal_mask(qseqlen, device=xq.device)
+                # better to pass attn_mask rather than compute so that it doesn't need to be computed at each layer?
+
+            # compute dot product
+            scores = torch.matmul(xq, xk.transpose(2, 3)) * self.attn_scale
+
+            # if softmax activation, masking is handled by adding -inf before softmax
+            if attn_mask is not None:
+                attn_mask_ = torch.zeros(qseqlen, kseqlen, dtype=xq.dtype, device=xq.device).masked_fill(attn_mask.logical_not(), float('-inf'))
+                scores = scores + attn_mask_
+
+            # apply softmax activation to inner products
+            scores = torch.nn.functional.softmax(scores, dim=-1)
+
+            scores = self.attn_dropout(scores)
+            output = torch.matmul(scores, xv)  # (bs, n_local_heads, seqlen, head_dim)
+
+        # restore time as batch dimension and concat heads
+        output = output.transpose(1, 2).contiguous().view(bsz, qseqlen, -1)
+
+        # final projection into the residual stream
+        output = self.wo(output)
+        output = self.resid_dropout(output)
+
+        return output, scores
+
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self,
+            embed_dim: int,
+            dff: int = None,
+            activation: str = 'relu',
+            use_bias: bool = False):
+        """
+        Feed-forward block.
+
+        A 2-layer neural network with activation function in between.
+
+        Parameters
+        ----------
+        embed_dim : int
+            embedding dimension of input.
+        dff : int, optional
+            size of intermediate layer. if None, 4 * embed_dim.
+        activation : str, optional
+            name of activation function, by default 'relu'
+        use_bias : bool, optional
+            whether to use bias in linear layers, by default False
+        """
+
+        super().__init__()
+        self.embed_dim = embed_dim
+
+        # set dff according to activation function if not given
+        if dff is None and activation == 'swiglu':
+            self.dff = int(2/3 * 4 * embed_dim)
+        elif dff is None:
+            self.dff = 4 * embed_dim
+        else:
+            self.dff = dff
+
+        self.use_bias = use_bias
+        self.activation = activation
+        if self.activation != 'swiglu':
+            self.activation_ = model_utils.get_activation_function(activation)
+
+        self.linear1 = nn.Linear(self.embed_dim, self.dff, bias=self.use_bias)
+        self.linear2 = nn.Linear(self.dff, self.embed_dim, bias=self.use_bias)
+        if self.activation == 'swiglu':
+            self.linear3 = nn.Linear(self.embed_dim, self.dff, bias=self.use_bias)
+
+    def forward(self, x):
+        if self.activation == 'swiglu':
+            return self.linear2(nn.functional.silu(self.linear1(x)) * self.linear3(x))
+        else:
+            x = self.linear1(x)
+            x = self.activation_(x)
+            x = self.linear2(x)
+            return x
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+def create_norm(d_model, norm_type):
+    if norm_type=='layernorm':
+        return nn.LayerNorm(d_model)
+    elif norm_type=='rmsnorm':
+        return RMSNorm(d_model)
+    elif norm_type=='none':
+        return  nn.Identity()
+    else:
+        raise ValueError(f'norm_type {norm_type} not valid')
+
+# Utilities associated with computing attention
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
+    bs, slen, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    return (
+        x[:, :, :, None, :]
+        .expand(bs, slen, n_kv_heads, n_rep, head_dim)
+        .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
+    )
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)  # type: ignore
+    freqs = torch.outer(t, freqs).float()  # type: ignore
+    freqs_cos = torch.cos(freqs)  # real part
+    freqs_sin = torch.sin(freqs)  # imaginary part
+    return freqs_cos, freqs_sin
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(shape)
+
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cos: torch.Tensor,
+    freqs_sin: torch.Tensor
+    ):
+
+    # reshape xq and xk to match the complex representation
+    xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
+    xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
+
+    # reshape freqs_cos and freqs_sin for broadcasting
+    freqs_cos = reshape_for_broadcast(freqs_cos, xq_r)
+    freqs_sin = reshape_for_broadcast(freqs_sin, xq_r)
+
+    # apply rotation using real numbers
+    xq_out_r = xq_r * freqs_cos - xq_i * freqs_sin
+    xq_out_i = xq_r * freqs_sin + xq_i * freqs_cos
+    xk_out_r = xk_r * freqs_cos - xk_i * freqs_sin
+    xk_out_i = xk_r * freqs_sin + xk_i * freqs_cos
+
+    # flatten last two dimensions
+    xq_out = torch.stack([xq_out_r, xq_out_i], dim=-1).flatten(3)
+    xk_out = torch.stack([xk_out_r, xk_out_i], dim=-1).flatten(3)
+
+    return xq_out.type_as(xq), xk_out.type_as(xk)
+
+# NOTE: be careful. pytorch API is inconsistent about whether True means attend or not attend. 
+# this works with the Attention module implemented above, but will only be compatible with some but not all pytorch implementations
+# e.g., works with nn.functional.scaled_dot_product_attention but not nn.MultiHeadAttention
+def compute_diag_mask(size, device=None):
+    """computes an attention mask with False on the diagonal and True elsewhere"""
+
+    diag_mask = torch.eye(size, device=device).logical_not()
+    # diag_mask = diag_mask.masked_fill(diag_mask == 1, float('-inf'))
+    return diag_mask
+
+def compute_causal_mask(size, device=None):
+    """computes an attention mask with True at (i,j) if i <= j"""
+    causal_mask = torch.tril(torch.ones(size, size, device=device)).bool()
+    return causal_mask
+
 
 # endregion
